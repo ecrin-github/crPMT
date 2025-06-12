c038a680275ee8cb49c64c479a84368f097c6d2a
cudillal <leopold.cudilla@ecrin.org>
2025-06-12 10:10:59 +0200

    Fix country flag emojis not displayed on Chromium

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import * as $ from 'jquery';
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+
 
 if (environment.production) {
   enableProdMode();
 }
+
+polyfillCountryFlagEmojis();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
